eff66ceaf4ebf6aa24ffb9b6e2189c154a5f8523
Rivka Ben Shalom <rivka@linnovate.net>
2013-02-04 16:10:12 +0200

    update

--- bookmark-tabs/chrome/src/get_opened_tabs_urls.js
+++ bookmark-tabs/chrome/src/get_opened_tabs_urls.js
@@ -1,50 +1,34 @@
-  function myDate()
-  {
+
+
+function similarDate() {
   var now = new Date();
-  alert(now);
-  return now.getDate()+'/'+now.getMonth()+1+'/'+now.getFullYear();
-}
-function printBookmarks(id) {
- noww='extension';
-chrome.bookmarks.getChildren(id, function(children) {
-    children.forEach(function(bookmark) { 
-      if(bookmark.title==noww)
-         {
-         alert("רצונ"+bookmark.title);
-         bb=bookmark.id;
-          return 0;
-          }
-     
-     printBookmarks(bookmark.id);
-   });
- 
- });return 1;
-}
+    var sec=now.getSeconds();
+     if( sec < 10 )
+       sec='0'+sec;
+    return now.getDate()+'/'+now.getMonth()+1+'/'+now.getFullYear()+' '+now.getHours()+':'+now.getMinutes()+':'+sec;
 
-function list(tabs) {  
-//if(!printBookmarks('0'))
- chrome.bookmarks.create({
-                         'title': 'extension'},
-                        function(newFolder) {
-                                     myfolder=newFolder.id ;
-                                            });
-//else mm=bb;
-  now1=myDate(); 
- chrome.bookmarks.create({'parentId':myfolder,
-                         'title': now1},
-                        function(newFolder) {
-                                     mm=newFolder.id ;
-                                   });
-for (var i = 0; i < tabs.length; i++) 
-  {
-    alert(i);
-          my=tabs[i].url + '\n';
-        chrome.bookmarks.create({'parentId':mm,
-                         'title': my,
-                       'url': my});    
-       }
+}
+function getOpenTabs( tabs ) {  
+   var currDate = similarDate(); 
+  chrome.bookmarks.create( {
+    'title': currDate},
+    function ( newFolder1 ) {
+    currentmyfolderID = newFolder1.id ;
+    alert('p'+currentmyfolderID);});
+  for ( var i = 0; i < tabs.length; i++ ) {
+    var tabUrl   = tabs[i].url ;
+        var tabTitle = tabs[i].title ;
+alert(tabUrl); 
+        chrome.bookmarks.create({'parentId': currentmyfolderID,
+      'title': tabTitle,
+      'url': tabUrl});    
+  }
  }
+chrome.browserAction.onClicked.addListener(function(tab) {
+alert('yes');
+alert('book');
+     chrome.tabs.getAllInWindow(null, getOpenTabs);
+     alert('end');
 
-document.getElementById('copy').addEventListener('click', function(e) {
-    chrome.tabs.getAllInWindow(null, list);
- }); 
+chrome.tabs.executeScript(null, {file: "prompt.js"});
+});
