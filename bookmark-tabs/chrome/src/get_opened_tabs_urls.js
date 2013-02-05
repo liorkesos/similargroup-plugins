2aa946c7e8392f700f064510e0b24408f7603b06
Rivka Ben Shalom <rivka@linnovate.net>
2013-02-05 15:45:01 +0200

    finally

--- bookmark-tabs/chrome/src/get_opened_tabs_urls.js
+++ bookmark-tabs/chrome/src/get_opened_tabs_urls.js
@@ -5,30 +5,55 @@ function similarDate() {
     var sec=now.getSeconds();
      if( sec < 10 )
        sec='0'+sec;
-    return now.getDate()+'/'+now.getMonth()+1+'/'+now.getFullYear()+' '+now.getHours()+':'+now.getMinutes()+':'+sec;
+     var min=now.getMinutes();
+     if( min < 10 )
+       min='0'+min;
+     var hour=now.getHours();
+     if( hour < 10 )
+       hour='0'+hour;
+    return now.getDate()+'/'+now.getMonth()+1+'/'+now.getFullYear()+' '+hour+':'+min+':'+sec;
 
 }
 function getOpenTabs( tabs ) {  
    var currDate = similarDate(); 
+   var currentmyFolderID;
   chrome.bookmarks.create( {
+
     'title': currDate},
-    function ( newFolder1 ) {
-    currentmyfolderID = newFolder1.id ;
-    alert('p'+currentmyfolderID);});
+    function ( newFolder ) {
+     currentmyfolderID = newFolder.id ;
+    });
+  //alert(currentmyfolderID);
   for ( var i = 0; i < tabs.length; i++ ) {
-    var tabUrl   = tabs[i].url ;
-        var tabTitle = tabs[i].title ;
-alert(tabUrl); 
+        var tabUrl   = tabs[i].url ;
+        var tabTitle = tabs[i].title;
         chrome.bookmarks.create({'parentId': currentmyfolderID,
-      'title': tabTitle,
-      'url': tabUrl});    
+         'title': tabTitle,
+           'url': tabUrl});
   }
  }
 chrome.browserAction.onClicked.addListener(function(tab) {
-alert('yes');
-alert('book');
      chrome.tabs.getAllInWindow(null, getOpenTabs);
-     alert('end');
+     });
+/* Check unique directory
+function printBookmarks(id) {
+  chrome.bookmarks.getChildren(id, function(children) {
+    children.forEach(function(bookmark) { 
+    if( bookmark.title == noww ) {
+      bb = bookmark.id;
+      return 0;
+    }
+     printBookmarks(bookmark.id);
+   });
+ });return 1;
+}
+*/
 
-chrome.tabs.executeScript(null, {file: "prompt.js"});
-});
+/* Possible html5 local storage too store directory ids.
+function supports_html5_storage() {
+  try {
+    return 'localStorage' in window && window['localStorage'] !== null;
+  } catch (e) {
+    return false;
+  }
+}*/
